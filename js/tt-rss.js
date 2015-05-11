04a42e0c529c02b30299257765cd2b7c8003eb85
Ryan Nowakowski <tubaman@fattuba.com>
2015-05-10 22:46:16 -0500

    store the new session id in a cookie

--- js/tt-rss.js
+++ js/tt-rss.js
@@ -20,6 +20,7 @@ function TtRss(url) {
     return api({op: 'login', user: username, password: password})
       .then(function(data) {
         sid = data.content.session_id;
+        Ember.$.cookie('ttrss_api_sid', sid);
         return sid;
       });
   }
